c217d96da4dd43a2283b36993fa626cd699295a1
evilbaschdi <sebwalter@gmx.net>
2021-05-24 12:25:58 +0200

    updated guest os string mappings

--- VmMachineHwVersionUpdater.Core/Settings/GuestOsStringMapping.cs
+++ VmMachineHwVersionUpdater.Core/Settings/GuestOsStringMapping.cs
@@ -11,6 +11,8 @@ namespace VmMachineHwVersionUpdater.Core.Settings
         public GuestOsStringMapping()
             : base("Settings\\GuestOsStringMapping.json")
         {
+            //https://github.com/vmware/open-vm-tools/blob/master/open-vm-tools/lib/include/guest_os_tables.h
+            //https://gist.github.com/dcode/1003a83388d770b978f723e37ce09e42
         }
     }
 }
