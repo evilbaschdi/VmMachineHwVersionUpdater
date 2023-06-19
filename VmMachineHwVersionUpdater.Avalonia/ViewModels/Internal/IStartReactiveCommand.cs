9c26d130a91d67107021025d24ae0b3d3633c3e6
Sebastian Walter <sebwalter@gmx.net>
2023-06-19 19:50:52 +0200

    wel...

--- VmMachineHwVersionUpdater.Avalonia/ViewModels/Internal/IStartReactiveCommand.cs
+++ VmMachineHwVersionUpdater.Avalonia/ViewModels/Internal/IStartReactiveCommand.cs
@@ -1,8 +1,4 @@
-﻿using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
-using EvilBaschdi.About.Avalonia;
-
-namespace VmMachineHwVersionUpdater.Avalonia.ViewModels.Internal;
+﻿namespace VmMachineHwVersionUpdater.Avalonia.ViewModels.Internal;
 
 /// <inheritdoc />
 public interface IStartReactiveCommand : IReactiveCommandUnitRun
@@ -126,26 +122,5 @@ class AddEditAnnotationReactiveCommand : ReactiveCommandUnitRun, IAddEditAnnotat
     public override void Run()
     {
         throw new NotImplementedException();
-    }
-}
-
-/// <inheritdoc />
-public interface IAboutWindowReactiveCommand : IReactiveCommandUnitRun
-{
-}
-
-/// <inheritdoc cref="IAboutWindowReactiveCommand" />
-/// <inheritdoc cref="ReactiveCommandUnitRun" />
-public class AboutWindowReactiveCommand : ReactiveCommandUnitRun, IAboutWindowReactiveCommand
-{
-    /// <inheritdoc />
-    public override void Run()
-    {
-        var aboutWindow = App.ServiceProvider.GetRequiredService<AboutWindow>();
-        var mainWindow = Application.Current?.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop ? desktop.MainWindow : null;
-        if (mainWindow != null)
-        {
-            aboutWindow.ShowDialog(mainWindow);
-        }
     }
 }
