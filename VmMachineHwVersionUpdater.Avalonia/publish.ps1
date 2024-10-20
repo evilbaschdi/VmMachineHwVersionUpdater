5a9c194ffa6e843130a15dc3164efb854b28ce60
Sebastian Walter <sebwalter@gmx.net>
2024-10-20 16:01:58 +0200

    raised TargetFramework to net9.0

--- VmMachineHwVersionUpdater.Avalonia/publish.ps1
+++ VmMachineHwVersionUpdater.Avalonia/publish.ps1
@@ -1,2 +1,2 @@
-dotnet publish -c Release -o "C:\Apps\$((Get-Item .).Name)\x64" -r win-x64 -f net8.0 --no-self-contained
-dotnet publish -c Release -o "C:\Apps\$((Get-Item .).Name)\arm64" -r win-arm64 -f net8.0 --no-self-contained
+dotnet publish -c Release -o "C:\Apps\$((Get-Item .).Name)\x64" -r win-x64 -f net9.0 --no-self-contained
+dotnet publish -c Release -o "C:\Apps\$((Get-Item .).Name)\arm64" -r win-arm64 -f net9.0 --no-self-contained
