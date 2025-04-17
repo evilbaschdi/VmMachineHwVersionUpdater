a5c7de7c75ba31ad75f264c61b488c6ee83de802
Sebastian Walter <sebwalter@gmx.net>
2025-04-17 13:42:32 +0200

    corrected guest os string mapping

--- VmMachineHwVersionUpdater.Core/Settings/GuestOsStringMapping.cs
+++ VmMachineHwVersionUpdater.Core/Settings/GuestOsStringMapping.cs
@@ -1,7 +1,7 @@
 ﻿namespace VmMachineHwVersionUpdater.Core.Settings;
 
 /// <inheritdoc cref="SettingsFromJsonFile" />
-public class GuestOsStringMapping() : SettingsFromJsonFile("Settings\\GuestOsStringMapping.json"), IGuestOsStringMapping
+public class GuestOsStringMapping() : SettingsFromJsonFile("Settings/GuestOsStringMapping.json"), IGuestOsStringMapping
 {
     //https://github.com/vmware/open-vm-tools/blob/master/open-vm-tools/lib/include/guest_os_tables.h
     //https://gist.github.com/dcode/1003a83388d770b978f723e37ce09e42
