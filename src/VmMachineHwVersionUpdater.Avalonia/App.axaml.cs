19144988d7d82f24f41cf4045c1adc939689d8a5
Sebastian Walter <sebwalter@gmx.net>
2026-06-28 12:03:02 +0200

    optimized App

--- src/VmMachineHwVersionUpdater.Avalonia/App.axaml.cs
+++ src/VmMachineHwVersionUpdater.Avalonia/App.axaml.cs
@@ -1,38 +1,29 @@
-using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using EvilBaschdi.Core.Avalonia.DependencyInjection;
-using EvilBaschdi.Core.Avalonia.Themes;
 using VmMachineHwVersionUpdater.Avalonia.ViewModels;
 using VmMachineHwVersionUpdater.Avalonia.Views;
 
 namespace VmMachineHwVersionUpdater.Avalonia;
 
 /// <inheritdoc />
-public class App : Application
+public class App : ApplicationWithSplash
 {
     /// <inheritdoc />
     public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
     /// <inheritdoc />
-    public override void OnFrameworkInitializationCompleted()
+    protected override bool ResizeWithBorder400 => true;
+
+    /// <inheritdoc />
+    protected override void PreMainWindowCreation()
     {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            ThemeEngine.Initialize(this);
-
-            // Line below is needed to remove Avalonia data validation.
-            // Without this line you will get duplicate validations from both Avalonia and CT
-            var mainWindow = new MainWindow
-                             {
-                                 DataContext = ApplicationServices.GetRequiredService<MainWindowViewModel>()
-                             };
-
-            ThemeEngine.ApplyThemeToWindow(mainWindow, true);
-
-            desktop.MainWindow = mainWindow;
-        }
-
-        base.OnFrameworkInitializationCompleted();
+        ApplicationServices.AppName = Current?.Name;
     }
+
+    /// <inheritdoc />
+    protected override Window CreateMainWindow() => new MainWindow
+                                                    {
+                                                        DataContext = ApplicationServices.GetRequiredService<MainWindowViewModel>()
+                                                    };
 }
